b26e09547213293a21bd006f59d840a940a501a4
maxcolucci <maxcolucci@outlook.it>
2025-01-23 16:41:36 +0100

    First components implementation: Created header and filters component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {SideFilterComponent} from './components/side-filter/side-filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, SideFilterComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/components/side-filter/side-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-filter',
+  templateUrl: './side-filter.component.html',
+  standalone: true,
+  styleUrls: ['./side-filter.component.css']
+})
+export class SideFilterComponent {
+
+}
