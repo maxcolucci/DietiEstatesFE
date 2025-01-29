4ece9b5906b5fc2d2b1c6d6764f6f2dc53715e6f
firemonster16 <davide_ranavolo@libero.it>
2025-01-29 18:24:14 +0100

    added search filters in the side-filter: The filters already present have been modified and filters have been added for: the number of bathrooms, the floor of the apartment, the garage or parking space, the heating of the apartment and energy efficiency

--- src/app/components/side-filter/side-filter.component.ts
+++ src/app/components/side-filter/side-filter.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {
+  MatAccordion, MatExpansionModule,
+  MatExpansionPanel,
+  MatExpansionPanelDescription,
+  MatExpansionPanelTitle
+} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-side-filter',
   templateUrl: './side-filter.component.html',
+  imports: [MatSliderModule, MatExpansionPanel, MatAccordion, MatExpansionModule, MatExpansionPanelTitle, MatExpansionPanelDescription, MatIconModule],
   standalone: true,
   styleUrls: ['./side-filter.component.css']
 })
 export class SideFilterComponent {
+  readonly panelOpenState = signal(false);
 
 }
